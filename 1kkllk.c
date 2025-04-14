633fcd3a36eee671455257d39ee18f1a8c4f1366
YulikTheBest <yulik1464@gmail.com>
2025-04-14 23:03:20 +0300

    first

---
+++ 1kkllk.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int A, B, C;
+
+    // Введення значень змінних
+    printf("Введіть A, B, C: ");
+    scanf("%d %d %d", &A, &B, &C);
+
+    // Виведення змінених значень без використання проміжних змінних
+    printf("A = %d\n", C);
+    printf("B = %d\n", A);
+    printf("C = %d\n", B);
+
+    return 0;
+}
