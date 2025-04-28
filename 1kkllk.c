978fd0608a91f07b04426fcf4bd132b1bea781a8
Юліан <yulik1464@gmail.com>
2025-04-29 00:30:18 +0300

    1kkllk.c

--- 1kkllk.c
+++ 1kkllk.c
@@ -7,10 +7,15 @@ int main() {
     printf("Введіть A, B, C: ");
     scanf("%d %d %d", &A, &B, &C);
 
-    // Виведення змінених значень без використання проміжних змінних
-    printf("A = %d\n", C);
-    printf("B = %d\n", A);
-    printf("C = %d\n", B);
+    A = A + B + C;
+    B = A - (B + C);
+    C = A - (B + C);
+    A = A - (B + C);
+
+    // Виведення змінених значень
+    printf("A = %d\n", A);
+    printf("B = %d\n", B);
+    printf("C = %d\n", C);
 
     return 0;
 }
